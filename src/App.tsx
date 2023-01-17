fc64f064cfcbd0ae830a29c27be524cdfc567bfe
LeeYeongin <youngin5757@gmail.com>
2023-01-17 15:04:02 +0900

    first-commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Cart from './views/cart';
+import MyCourse from './views/myCourse';
+
+function App() {
+  return (
+    <MyCourse />
+  );
+}
+
+export default App;
