128295508f9a0f687b4eeeb157c6c9389090cdfe
NALASAI <ebyul96428@naver.com>
2023-01-20 13:32:20 +0900

    별점기능 추가[package-lock.json으로인해 깃에 올리는게 어려워 지는거 같습니다]

--- frontend/src/views/detail/board/boardWrite.tsx
+++ frontend/src/views/detail/board/boardWrite.tsx
@@ -1,30 +1,35 @@
-import React, { useState } from "react";
+import React, { ReactElement, useEffect } from "react";
+import Box from '@mui/material/Box';
+import Rating from '@mui/material/Rating';
 import './board.css';
 
 function BoardWrite() {
+  const [value, setValue] = React.useState<number | null>(5);
 
-  const Write = () => {
+  const WriteBtn = () => {
     window.location.href = "/main5";
-
     // 이동과 동시에 DB에 리뷰 저장
     // + 돌아갔을때 해당 페이지의 리뷰모음에 첫번째 자리에 들어있어야 한다.
   }
-
   return(
       <div className="boardBody5">
-          <div className="boardHead5">
-              <div className="board_ID"><span>아이디</span></div>
-              <div className="board_DATE"><span>날짜</span></div>
-          </div>
           <div className="boardTitle5">
-              <input type="text" placeholder="제목을 입력해주세요"/>
+            <div className="board_title">제목</div>
+            <div className="input_title"><input type="text" placeholder="제목을 입력해주세요"/></div>
+          </div>
+          <div className="boardHead5">
+              <div className="board_ID">아이디</div>
+              <div className="input_ID">로그인한 아이디</div>
           </div>
           <div className="boardText5">
             <textarea className="reviewArea" placeholder="리뷰를 작성해 주세요"/>
+            {/* 별점기능 */}
+            <Box sx={{'& > legend': { mt: 2 }}} className="starRating">
+              <Rating className="WriteRating" value={value} size='large' onChange={(event, newValue) => {setValue(newValue);}}/>
+            </Box> 
           </div>
           <div className="boardFooter">
-            <button type="button" className="writeBtn5" onClick={Write}>작성</button>
-            <button type="button" className="writeBtn5">수정하기</button>
+            <button type="button" className="writeBtn5" onClick={WriteBtn}>작성</button>
           </div>
       </div>
   );
