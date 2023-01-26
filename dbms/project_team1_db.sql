d82aa851c76ead89f52e288a6ced23adba828c8e
LeeYeongin <youngin5757@gmail.com>
2023-01-26 11:34:51 +0900

    데이터베이스 수정

--- dbms/project_team1_db.sql
+++ dbms/project_team1_db.sql
@@ -23,25 +23,13 @@ USE `project_team1`;
 -- 테이블 project_team1.cart 구조 내보내기
 DROP TABLE IF EXISTS `cart`;
 CREATE TABLE IF NOT EXISTS `cart` (
-  `id_cart` int NOT NULL,
+  `id_cart` int NOT NULL AUTO_INCREMENT,
   `id_user` varchar(45) NOT NULL,
   `id_class` int NOT NULL,
   PRIMARY KEY (`id_cart`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- 테이블 데이터 project_team1.cart:~0 rows (대략적) 내보내기
-DELETE FROM `cart`;
-
--- 테이블 project_team1.category 구조 내보내기
-DROP TABLE IF EXISTS `category`;
-CREATE TABLE IF NOT EXISTS `category` (
-  `id_category` int NOT NULL,
-  `category_name` varchar(45) NOT NULL,
-  PRIMARY KEY (`id_category`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- 테이블 데이터 project_team1.category:~0 rows (대략적) 내보내기
-DELETE FROM `category`;
+-- 내보낼 데이터가 선택되어 있지 않습니다.
 
 -- 테이블 project_team1.class_dtl 구조 내보내기
 DROP TABLE IF EXISTS `class_dtl`;
@@ -50,133 +38,120 @@ CREATE TABLE IF NOT EXISTS `class_dtl` (
   `img` varchar(45) NOT NULL,
   `class_name` varchar(45) NOT NULL,
   `price` int NOT NULL,
-  `student_num` varchar(45) NOT NULL,
+  `student_count` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `class_info_simple` varchar(45) NOT NULL,
   `class_info_dtl` varchar(45) NOT NULL,
+  `category` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `difficulty` varchar(50) NOT NULL,
+  `discount_rate` int DEFAULT NULL,
   PRIMARY KEY (`id_class`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- 테이블 데이터 project_team1.class_dtl:~0 rows (대략적) 내보내기
-DELETE FROM `class_dtl`;
+-- 내보낼 데이터가 선택되어 있지 않습니다.
 
 -- 테이블 project_team1.class_mst 구조 내보내기
 DROP TABLE IF EXISTS `class_mst`;
 CREATE TABLE IF NOT EXISTS `class_mst` (
   `id_class` int NOT NULL,
-  `id_category` int NOT NULL,
-  `id_difficulty` int NOT NULL,
   `id_instructor` int NOT NULL,
-  `id_discount` int NOT NULL,
   PRIMARY KEY (`id_class`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- 테이블 데이터 project_team1.class_mst:~0 rows (대략적) 내보내기
-DELETE FROM `class_mst`;
+-- 내보낼 데이터가 선택되어 있지 않습니다.
 
 -- 테이블 project_team1.curriculum 구조 내보내기
 DROP TABLE IF EXISTS `curriculum`;
 CREATE TABLE IF NOT EXISTS `curriculum` (
-  `id_curriculum` int NOT NULL,
-  `section` varchar(45) NOT NULL,
-  `part` varchar(45) NOT NULL,
-  `video` varchar(200) NOT NULL,
-  `id_class` int NOT NULL,
+  `id_curriculum` int NOT NULL AUTO_INCREMENT,
+  `section` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `part` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `video` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `id_class` int DEFAULT NULL,
   PRIMARY KEY (`id_curriculum`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- 테이블 데이터 project_team1.curriculum:~0 rows (대략적) 내보내기
-DELETE FROM `curriculum`;
-
--- 테이블 project_team1.difficulty 구조 내보내기
-DROP TABLE IF EXISTS `difficulty`;
-CREATE TABLE IF NOT EXISTS `difficulty` (
-  `id_difficulty` int NOT NULL,
-  `difficulty_name` varchar(45) NOT NULL,
-  PRIMARY KEY (`id_difficulty`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- 테이블 데이터 project_team1.difficulty:~0 rows (대략적) 내보내기
-DELETE FROM `difficulty`;
-
--- 테이블 project_team1.discount 구조 내보내기
-DROP TABLE IF EXISTS `discount`;
-CREATE TABLE IF NOT EXISTS `discount` (
-  `id_discount` int NOT NULL,
-  `discount_rate` int DEFAULT NULL,
-  PRIMARY KEY (`id_discount`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- 테이블 데이터 project_team1.discount:~0 rows (대략적) 내보내기
-DELETE FROM `discount`;
+-- 내보낼 데이터가 선택되어 있지 않습니다.
 
 -- 테이블 project_team1.instructor 구조 내보내기
 DROP TABLE IF EXISTS `instructor`;
 CREATE TABLE IF NOT EXISTS `instructor` (
-  `id_instructor` int NOT NULL,
+  `id_instructor` int NOT NULL AUTO_INCREMENT,
   `instructor_name` varchar(45) NOT NULL,
-  `instructor_info` varchar(45) NOT NULL,
-  `instructor_img` varchar(45) NOT NULL,
+  `instructor_info` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `instructor_img` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
   PRIMARY KEY (`id_instructor`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- 테이블 데이터 project_team1.instructor:~0 rows (대략적) 내보내기
-DELETE FROM `instructor`;
+-- 내보낼 데이터가 선택되어 있지 않습니다.
 
 -- 테이블 project_team1.order_dtl 구조 내보내기
 DROP TABLE IF EXISTS `order_dtl`;
 CREATE TABLE IF NOT EXISTS `order_dtl` (
-  `id_order` int NOT NULL,
-  `id_class` int NOT NULL,
-  `price` int NOT NULL,
+  `id_order` int NOT NULL AUTO_INCREMENT,
+  `id_class` int DEFAULT NULL,
+  `price` int DEFAULT NULL,
   PRIMARY KEY (`id_order`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- 테이블 데이터 project_team1.order_dtl:~0 rows (대략적) 내보내기
-DELETE FROM `order_dtl`;
+-- 내보낼 데이터가 선택되어 있지 않습니다.
 
 -- 테이블 project_team1.order_mst 구조 내보내기
 DROP TABLE IF EXISTS `order_mst`;
 CREATE TABLE IF NOT EXISTS `order_mst` (
-  `id_order` int NOT NULL,
+  `id_order` int NOT NULL AUTO_INCREMENT,
   `order_date` date NOT NULL,
-  `id_user` varchar(45) NOT NULL,
+  `id_user` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `status` varchar(45) NOT NULL,
   PRIMARY KEY (`id_order`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- 테이블 데이터 project_team1.order_mst:~0 rows (대략적) 내보내기
-DELETE FROM `order_mst`;
+-- 내보낼 데이터가 선택되어 있지 않습니다.
 
 -- 테이블 project_team1.review 구조 내보내기
 DROP TABLE IF EXISTS `review`;
 CREATE TABLE IF NOT EXISTS `review` (
-  `id_review` int NOT NULL,
-  `title` varchar(45) NOT NULL,
-  `id_user` varchar(45) NOT NULL,
+  `id_review` int NOT NULL AUTO_INCREMENT,
+  `id_user` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `contents` text NOT NULL,
   `id_class` int NOT NULL,
-  `grade` float NOT NULL,
+  `grade` int NOT NULL DEFAULT '0',
   PRIMARY KEY (`id_review`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- 테이블 데이터 project_team1.review:~0 rows (대략적) 내보내기
-DELETE FROM `review`;
+-- 내보낼 데이터가 선택되어 있지 않습니다.
 
 -- 테이블 project_team1.user 구조 내보내기
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
-  `id` varchar(20) NOT NULL,
+  `id` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `password` varchar(45) NOT NULL,
   `name` varchar(45) NOT NULL,
-  `profile` varchar(45) DEFAULT NULL,
+  `profile` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
   `nickname` varchar(45) NOT NULL,
   `email` varchar(45) NOT NULL,
   `telnum` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- 테이블 데이터 project_team1.user:~0 rows (대략적) 내보내기
-DELETE FROM `user`;
+-- 내보낼 데이터가 선택되어 있지 않습니다.
+
+-- 트리거 project_team1.order_mst_after_insert 구조 내보내기
+DROP TRIGGER IF EXISTS `order_mst_after_insert`;
+SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+DELIMITER //
+CREATE TRIGGER `order_mst_after_insert` AFTER INSERT ON `order_mst` FOR EACH ROW BEGIN
+	INSERT into
+
+		order_dtl
+
+		(id_order)
+
+	values
+
+		(NEW.id_order);
+END//
+DELIMITER ;
+SET SQL_MODE=@OLDTMP_SQL_MODE;
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
