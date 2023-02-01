aef4dbaf175b5b7c4b81a1355a7257b036979c86
karatejin <junsyang@naver.com>
2023-02-01 11:12:43 +0900

    2023-02-01"Still refix Carousel"

--- frontend/src/views/main/FrontCarousel.tsx
+++ frontend/src/views/main/FrontCarousel.tsx
@@ -1,10 +1,9 @@
-import React, { Component, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Rating from '@mui/material/Rating';
 import axios from "axios";
-// import { useEffect } from "react";
 
 import '../main/css/CarouselArrow.css';
 
@@ -19,70 +18,52 @@ import { useParams } from "react-router-dom";
     return(
       <Rating name="read-only" value={value} readOnly />
     )
-  }
-
-  const [connection, setConnection] = useState<string>('');
-
-  const connectionTest = () =>{
-    axios.get('http://localhost:4040/front').then((response) => {
-      setConnection("success!");
-
-    }).catch((error)=>{
-      setConnection(error.message);
-    })
-  }
-
-  useEffect(() => {
-    connectionTest();
-  },[]);
-
-
-    
+  }    
   const CarouselMap = [
 
   {
-      category:'connection',
+      category:'프론트엔드',
       subtitle: 'forntend',
       lectures:[
               {
                   img: "",
                   title:"프론트엔드",
                   name: "프론트강사",
-                  rating: 3,
-                  price: 17000,
-                  tag: true
+                  rating: "",
+                  price: "",
+                  studentCount: ""
               },
               {
                   img: "",
                   title:"프론트엔드",
                   name: "프론트강사",
-                  rating: 3,
-                  price: 17000,
-                  tag: true
+                  rating: "",
+                  price: "",
+                  studentCount: ""
               },
               {
                   img: "",
                   title:"프론트엔드",
                   name: "프론트강사",
-                  rating: 3,
-                  price: 17000,
-                  tag: true
+                  rating: "",
+                  price: "",
+                  studentCount: ""
               },
               {
                   img: "",
                   title:"프론트엔드",
                   name: "프론트강사",
-                  rating: 3,
-                  price: 17000,
-                  tag: true
+                  rating: "",
+                  price: "",
+                  studentCount: ""
               },
               {
                   img: "",
                   title:"프론트엔드",
                   name: "프론트강사",
-                  rating: 3,
-                  price: 17000,
-                  tag: true
+                  rating: "",
+                  price: "",
+                  studentCount: ""
               }
       ]
   },
@@ -94,41 +75,41 @@ import { useParams } from "react-router-dom";
               img: "",
               title:"백엔드",
               name: "백엔드강사",
-              rating: 3,
-              price: 17000,
-              tag: true
+              rating: "",
+              price: "",
+              studentCount: ""
           },
           {
               img: "",
               title:"백엔드",
               name: "백엔드강사",
-              rating: 3,
-              price: 17000,
-              tag: true
+              rating: "",
+              price: "",
+              studentCount: ""
           },
           {
               img: "",
               title:"백엔드",
               name: "백엔드강사",
-              rating: 3,
-              price: 17000,
-              tag: true
+              rating: "",
+              price: "",
+              studentCount: ""
           },
           {
               img: "",
               title:"백엔드",
               name: "백엔드강사",
-              rating: 3,
-              price: 17000,
-              tag: true
+              rating: "",
+              price: "",
+              studentCount: ""
           },
           {
               img: "",
               title:"백엔드",
               name: "백엔드강사",
-              rating: 3,
-              price: 17000,
-              tag: true
+              rating: "",
+              price: "",
+              studentCount: ""
           },
       ]
   },
@@ -140,41 +121,41 @@ import { useParams } from "react-router-dom";
               img: "",
               title:"데이터베이스",
               name: "데이터베이스강사",
-              rating: 3,
-              price: 17000,
-              tag: true
+              rating: "",
+              price: "",
+              studentCount: ""
           },
           {
               img: "",
               title:"데이터베이스",
               name: "데이터베이스강사",
-              rating: 3,
-              price: 17000,
-              tag: true
+              rating: "",
+              price: "",
+              studentCount: ""
           },
           {
               img: "",
               title:"데이터베이스",
               name: "데이터베이스강사",
-              rating: 3,
-              price: 17000,
-              tag: true
+              rating: "",
+              price: "",
+              studentCount: ""
           },
           {
               img: "",
               title:"데이터베이스",
               name: "데이터베이스강사",
-              rating: 3,
-              price: 17000,
-              tag: true
+              rating: "",
+              price: "",
+              studentCount: ""
           },
           {
               img: "",
               title:"데이터베이스",
               name: "데이터베이스강사",
-              rating: 3,
-              price: 17000,
-              tag: true
+              rating: "",
+              price: "",
+              studentCount: ""
           }
       ]
   },
@@ -186,47 +167,48 @@ import { useParams } from "react-router-dom";
               img: "",
               title:"풀 스택",
               name: "풀 스택강사",
-              rating: 3,
-              price: 17000,
-              tag: true
+              rating: "",
+              price: "",
+              studentCount: ""
           },
           {
               img: "",
               title:"풀 스택",
               name: "풀 스택강사",
-              rating: 3,
-              price: 17000,
-              tag: true
+              rating: "",
+              price: "",
+              studentCount: ""
           },
           {
               img: "",
               title:"풀 스택",
               name: "풀 스택강사",
-              rating: 3,
-              price: 17000,
-              tag: true
+              rating: "",
+              price: "",
+              studentCount: ""
           },
           {
               img: "",
               title:"풀 스택",
               name: "풀 스택강사",
-              rating: 3,
-              price: 17000,
-              tag: true
+              rating: "",
+              price: "",
+              studentCount: ""
           },
           {
               img: "",
               title:"풀 스택",
               name: "풀 스택강사",
-              rating: 3,
-              price: 17000,
-              tag: true
+              rating: "",
+              price: "",
+              studentCount: ""
           },
       ]
   }
 ]
 
-const STUDY = CarouselMap.map((cm) => {
+const STUDY = CarouselMap.map((cm) => {  
+
   const settings = {
     dots: false,
     infinite: false,
@@ -238,7 +220,7 @@ const STUDY = CarouselMap.map((cm) => {
     <div className="course_dashboard_list4">
     <div className="course_dashboard_title4">
       <div key = {cm.category}>
-          <p>{connection}</p>
+          <h3>{cm.category}</h3>
           <p style={{ fontSize: "13px" }}>{cm.subtitle}</p>
       </div>
     </div>
@@ -246,16 +228,15 @@ const STUDY = CarouselMap.map((cm) => {
       <Slider {...settings}>
         {cm.lectures.map((item) => (
           <a href="/main5">
-            <div className="course_card4">
-          <img src={Course1} alt="" className="course_face4" />
-          <div className="course_title4">{item.title}</div>
-          <div className="course_instructor4">{item.name}</div>
-          <div className="course_rating4"><ReadOnly/></div>
-          <div className="course_price4">{item.price} 원</div>
-          <div className="course_tag4">{item.tag && '베스트셀러'}</div>
-        </div>
+          <div className="course_card4">
+            <img src={Course1} alt="" className="course_face4" />
+            <div className="course_title4">{item.title}</div>
+            <div className="course_instructor4">{item.name}</div>
+            <div className="course_rating4"><ReadOnly/></div>
+            <div className="course_price4">{item.price} 원</div>
+            <div className="course_studentCount4">{item.studentCount}</div>
+          </div>
           </a>
-          
         ))}
       </Slider>
     </div>
@@ -263,14 +244,48 @@ const STUDY = CarouselMap.map((cm) => {
   )
 })
 
-export default class CustomCarousel extends Component {
-  render() {
-    
+export default function CustomCarousel() {
+
+  const [cm, setcm] = useState<any[]>([]);
+
+  
+  const [connection, setConnection] = useState<any>();
+
+  const connectionTest = () =>{
+    axios.get('http://localhost:4040/front').then((Response) => {
+      const tmp = [];
+
+      console.log(Response.data);
+      for (let i = 0; i < Response.data.data.length; i++){
+        tmp.push({
+            idClass: Response.data.data[i].idClass,
+            img: Response.data.data[i].img,
+            className: Response.data.data[i].className,
+            instructor: Response.data.data[i].instructor,
+            grade: Response.data.data[i].grade,
+            price: Response.data.data[i].price + "원",
+            studentCount: Response.data.data[i].studentCount + "명"
+
+          })
+          console.log(Response.data.data[0].className);
+          setcm(tmp);
+      }
+      setConnection(Response.data.data);
+
+    }).catch((error)=>{
+      setConnection(error.message);
+    })
+  }
+
+  useEffect(() => {
+    connectionTest();
+  },[]);
 
     return (
       <div className="course_dashboard4">
         {STUDY}
+        {connection}
       </div>
     );
-  }
+  
 }
