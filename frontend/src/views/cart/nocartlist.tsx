1900ede2d41c96f1e8885d0b4625775f6656f8dd
LeeYeongin <youngin5757@gmail.com>
2023-02-02 12:47:02 +0900

    수강바구니 버튼 링크수정

--- frontend/src/views/cart/nocartlist.tsx
+++ frontend/src/views/cart/nocartlist.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function NoCartList() {
   return (
     <div className='btn-container2'>
-        <a href="/">
+        <a href="/myProfile">
             <button className='show-more-class-btn'>강의 더 보기</button>
         </a>
     </div>
