f9e977274d72a0888fe68036c00a23f8a6371741
NALASAI <ebyul96428@naver.com>
2023-02-08 13:15:15 +0900

    전체선택기능(미완)

--- frontend/src/views/cart/cartlist.tsx
+++ frontend/src/views/cart/cartlist.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 
 interface props {
     itemList: any[];
@@ -9,6 +10,10 @@ interface props {
 }
 
 export default function CartList({itemList, checkValue, setCheckValue, setPriceSum, priceSum}: props) {
+  const [check, setCheck] = useState<boolean>(false);
+  const [isAllCheck, setIsAllCheck] = useState<boolean>(false);
+  const [requestResult, setRequestResult] = useState<string>('')
+
   const plusPriceFucntion = (price: number) => {
     let sum = priceSum;
     sum = sum + price;
@@ -21,51 +26,99 @@ export default function CartList({itemList, checkValue, setCheckValue, setPriceS
     setPriceSum(sum)
   }
 
-    const cartCheckHandler = (data: any) => {
-        let tmp = checkValue;
-        const obj = JSON.parse(data);
-        let id = obj.idCart
-        if (tmp.includes(id)){
-          tmp = checkValue.filter((item) => item !== id);
-          minusPriceFucntion(parseInt(obj.price))
-        }
-        else{
-          tmp.push(id);
-          plusPriceFucntion(parseInt(obj.price))
-        } 
-        setCheckValue(tmp);
-        console.log(checkValue);
+  const handleCartCheckAllHandler = (data: any, checked:boolean) => {
+    if(checked){
+      const idArray: any[] = [];
+      itemList.forEach((el:any) => idArray.push(el.idCart));
+      setCheckValue(idArray);
+
+      let tmp = checkValue;
+      const obj = JSON.parse(data);
+      let id = obj.idCart;
+      if (tmp.includes(id)){
+        tmp = checkValue.filter((item) => item !== id);
+        minusPriceFucntion(parseInt(obj.price))
       }
+      else{
+        tmp.push(id);
+        plusPriceFucntion(parseInt(obj.price))
+      }
+      setCheckValue(tmp);
+
+    }else{
+      setCheckValue([]);
+    }
+  }
+
+  const cartCheckHandler = (data: any, checked:boolean) => {
+    if(checked){
+      let tmp = checkValue;
+      const obj = JSON.parse(data);
+      let id = obj.idCart;
+      if (tmp.includes(id)){
+        tmp = checkValue.filter((item) => item !== id);
+        minusPriceFucntion(parseInt(obj.price))
+      }
+      else{
+        tmp.push(id);
+        plusPriceFucntion(parseInt(obj.price))
+      }
+      setCheckValue(tmp);
+    }else{
+      setCheckValue(checkValue.filter((el:any) => el !== checkValue));
+    }
+  }
+
+  const deleteHandler = () => {
+    axios.post("http://localhost:4040/cart/delete", checkValue)
+    .then((Response) => {
+      setRequestResult('Success!!');
+      setCheckValue([])
+    })
+    .catch((error) => {
+      setRequestResult('Failed!!');
+    })
+    console.log(requestResult);
+  }
 
   return (
     <>
-        {itemList.map((item) => (
-                <>
-                <div className="cart-list2">
-                  <input type="checkbox" value={JSON.stringify(item)} onChange={(e) => {cartCheckHandler(e.currentTarget.value)}} className="course-select2" />
-                  <div className="cart-course-img2">
-                    <img src={item.img} alt="course-img" />
-                  </div>
-                  <div className="cart-course-info2">
-                    <h3 className="course-title2">
-                      <a href="#">{item.className}</a>
-                    </h3>
-                    <div>
-                      <span>{item.instructor}</span>
-                    </div>
-                  </div>
-                  <div className="close2">
-                    <button className="close-btn2">
-                      <i className="fa-solid2 fa-xmark2"></i>
-                    </button>
-                  </div>
-                  <div className="payment2">
-                    {item.price}원
-                  </div>
-                </div>
-              </>
-              ))}
+      <div className="cart-control2">
+        <div className="select-control2">
+          <input type="checkbox"
+          checked={checkValue.length === itemList.length ? true : false}
+          onChange={(e)=>handleCartCheckAllHandler(itemList.map((item) => {JSON.stringify(item)}), e.target.checked)}/>
+          <span> 전체선택</span>
+        </div>
+        <button className="cancel-control2" onClick={() => deleteHandler()}>선택삭제 X</button>
+      </div>
+      {itemList.map((item) => (
+        <div className="cart-list2">
+          <input type="checkbox" value={JSON.stringify(item)}
+          onChange={(e) => {cartCheckHandler(e.currentTarget.value, e.target.checked)}}
+          checked={checkValue.includes(item.idCart) ? true : false} 
+          className="course-select2"/>
+          <div className="cart-course-img2">
+            <img src={item.img} alt="course-img" />
+          </div>
+          <div className="cart-course-info2">
+            <h3 className="course-title2">
+              <a href="#">{item.className}</a>
+            </h3>
+            <div>
+              <span>{item.instructor}</span>
+            </div>
+          </div>
+          <div className="close2">
+            <button className="close-btn2">
+              <i className="fa-solid2 fa-xmark2"></i>
+            </button>
+          </div>
+          <div className="payment2">
+            {item.price}원
+          </div>
+        </div>
+      ))}
     </>
-
   )
 }
--- frontend/src/views/cart/index.tsx
+++ frontend/src/views/cart/index.tsx
@@ -15,25 +15,10 @@ export default function Cart() {
   const [checkValue, setCheckValue] = useState<number[]>([]);
   const [priceSum, setPriceSum] = useState<number>(0);
   const [showList, setShowList] = useState<boolean>(false);
-  const [isAllCheck, setIsAllCheck] = useState<boolean>(true);
-
-  const deleteHandler = () => {
-    axios.post("http://localhost:4040/cart/delete", checkValue)
-    .then((Response) => {
-      setRequestResult('Success!!');
-      setCheckValue([])
-    })
-    .catch((error) => {
-      setRequestResult('Failed!!');
-    })
-
-    cartHandler();
-    showListHandler();
-  }
 
   const cartHandler = () => {
     const getdata = {
-      idUser: "ccc"
+      idUser: "aaa"
     };
     
     axios.post("http://localhost:4040/cart/", getdata)
@@ -91,207 +76,60 @@ export default function Cart() {
     cartHandler()
   },[])
 
-  
-  
   return (
-    <>
-      <div className="container-wrapper2">
-        <div className="container2">
-          <div className="cart-section2">
-            <h1 className="cart-name2">수강바구니</h1>
-            <div className="cart-container2">
-              <>
-              <div className="cart-control2">
-                <div className="select-control2">
-                  <input type="checkbox" checked={isAllCheck} onClick={()=>setIsAllCheck(isAllCheck?false:true)}/>
-                  <span> 전체선택</span>
-                </div>
-                <button className="cancel-control2" onClick={() => deleteHandler()}>선택삭제 X</button>
-              </div>
-              {showListHandler()}
-              </>
-            </div>
-          </div>
-          <div className="side-container2">
-            <div className="buyer-info2">
-              <div className="buyer-info-header2">
-                <div>
-                  <span>구매자정보</span>
-                  <i className="fa-regular2 fa-circle-question2"></i>
-                </div>
-              </div>
-              <div className="buyer-name2">
-                <dt>이름</dt>
-                <dd>{name}</dd>
-              </div>
-              <div className="buyer-email2">
-                <dt>이메일</dt>
-                <dd>{email}</dd>
-              </div>
-              <div className="buyer-telphone2">
-                <dt>전화번호</dt>
-                <dd>{telNum}</dd>
-              </div>
-            </div>
-            <div className="payment-container2">
-              {/* <div className="coupon-using2">
-                <div className="coupon-header2">
-                  <div>
-                    <span>쿠폰</span>
-                    <i className="fa-regular2 fa-circle-question2"></i>
-                  </div>
-                  <div>
-                    <span>사용가능</span>
-                    <span>0</span>
-                  </div>
-                </div>
-                <div className="coupon-select2">
-                  <input
-                    type="text"
-                    value="0"
-                    className="coupon-input2"
-                    disabled
-                  />
-                  <button className="coupon-select-btn2">쿠폰선택</button>
-                </div>
-              </div>
-              <div className="point-using2">
-                <div className="point-header2">
-                  <div>
-                    <span>포인트</span>
-                    <i className="fa-regular2 fa-circle-question2"></i>
-                  </div>
-                  <div>
-                    <span>보유</span>
-                    <span>0</span>
-                  </div>
-                </div>
-                <div className="point-select2">
-                  <input
-                    type="text"
-                    placeholder="1000포인트 이상 사용가능"
-                    className="point-input2"
-                  />
-                  <button className="point-select-btn2">전액사용</button>
-                </div>
-              </div> */}
-              <div className="price-regular2">
-                <span>선택상품 금액</span>
-                <span>{priceSum}원</span>
-              </div>
-              <div className="price-discount2">
-                <div>
-                  <span>할인금액</span>
-                  <button className="show-detail2">
-                    <i className="fa-solid2 fa-angle-down2"></i>
-                  </button>
-                </div>
-                <span>0원</span>
-              </div>
-              <div className="price-pay2">
-                <span>총 결제금액</span>
-                <span>{priceSum}원</span>
-                {/* <div>{requestResult}</div> */}
-              </div>
-              <button className="payment-btn2" onClick={() => cartHandler()}>
-                결제하기
-              </button>
-            </div>
+    <div className="container-wrapper2">
+      <div className="container2">
+        <div className="cart-section2">
+          <h1 className="cart-name2">수강바구니</h1>
+          <div className="cart-container2">
+            {showListHandler()}
           </div>
         </div>
-        {/* <div className="recommend-container2">
-          <h1 className="recommend-list-title2">
-            나와 비슷한 사람들이 수강한 강의
-          </h1>
-          <div className="recommend-list2">
-            <div className="recommend-cousrse-card-wrapper2">
-              <div className="recommend-course-card2">
-                <div className="card-img2">
-                  <img src="img/강아지.jpg" alt="" className="card-img img2" />
-                </div>
-                <div className="recommend-card-content2">
-                  <div className="course-title2">나도코딩의 자바 기본편</div>
-                  <div className="course-instructor2">나도 코딩</div>
-                  <span className="course-price2">50,050원</span>
-                </div>
-              </div>
-              <div className="add-cart2">
-                <div className="add-cart-btn-container2">
-                  <div className="add-cart-course-info2">
-                    <a href="https://www.naver.com/">나도코딩의 자바 기본편</a>
-                    <br />
-                    <span>나도 코딩</span>
-                  </div>
-                  <button
-                    type="button"
-                    className="cart-btn2"
-                    onClick={() => {}}
-                  >
-                    <i className="fa-solid2 fa-cart-plus2"></i>
-                  </button>
-                </div>
+        <div className="side-container2">
+          <div className="buyer-info2">
+            <div className="buyer-info-header2">
+              <div>
+                <span>구매자정보</span>
+                <i className="fa-regular2 fa-circle-question2"></i>
               </div>
             </div>
-            <div className="recommend-cousrse-card-wrapper2">
-              <div className="recommend-course-card2">
-                <div className="card-img2">
-                  <img src={Totoro} alt="" className="card-img img2" />
-                </div>
-                <div className="recommend-card-content2">
-                  <div className="course-title2">나도코딩의 자바 기본편</div>
-                  <div className="course-instructor2">나도 코딩</div>
-                  <span className="course-price2">50,050원</span>
-                </div>
-              </div>
-              <div className="add-cart2">
-                <div className="add-cart-btn-container2">
-                  <div className="add-cart-course-info2">
-                    <a href="https://www.naver.com/">나도코딩의 자바 기본편</a>
-                    <br />
-                    <span>나도 코딩</span>
-                  </div>
-                  <button
-                    type="button"
-                    className="cart-btn2"
-                    onClick={() => {}}
-                  >
-                    <i className="fa-solid2 fa-cart-plus2"></i>
-                  </button>
-                </div>
-              </div>
+            <div className="buyer-name2">
+              <dt>이름</dt>
+              <dd>{name}</dd>
             </div>
-
-            <div className="recommend-cousrse-card-wrapper2">
-              <div className="recommend-course-card2">
-                <div className="card-img2">
-                  <img src="img/단풍.jpg" alt="" className="card-img img2" />
-                </div>
-                <div className="recommend-card-content2">
-                  <div className="course-title2">나도코딩의 자바 기본편</div>
-                  <div className="course-instructor2">나도 코딩</div>
-                  <span className="course-price2">50,050원</span>
-                </div>
-              </div>
-              <div className="add-cart2">
-                <div className="add-cart-btn-container2">
-                  <div className="add-cart-course-info2">
-                    <a href="https://www.naver.com/">나도코딩의 자바 기본편</a>
-                    <br />
-                    <span>나도 코딩</span>
-                  </div>
-                  <button
-                    type="button"
-                    className="cart-btn2"
-                    onClick={() => {}}
-                  >
-                    <i className="fa-solid2 fa-cart-plus2"></i>
-                  </button>
-                </div>
-              </div>
+            <div className="buyer-email2">
+              <dt>이메일</dt>
+              <dd>{email}</dd>
+            </div>
+            <div className="buyer-telphone2">
+              <dt>전화번호</dt>
+              <dd>{telNum}</dd>
             </div>
           </div>
-        </div> */}
+          <div className="payment-container2">
+            <div className="price-regular2">
+              <span>선택상품 금액</span>
+              <span>{priceSum}원</span>
+            </div>
+            <div className="price-discount2">
+              <div>
+                <span>할인금액</span>
+                <button className="show-detail2">
+                  <i className="fa-solid2 fa-angle-down2"></i>
+                </button>
+              </div>
+              <span>0원</span>
+            </div>
+            <div className="price-pay2">
+              <span>총 결제금액</span>
+              <span>{priceSum}원</span>
+            </div>
+            <button className="payment-btn2" onClick={() => cartHandler()}>
+              결제하기
+            </button>
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
